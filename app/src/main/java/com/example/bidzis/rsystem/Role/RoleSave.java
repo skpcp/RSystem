dae126a8573a6c3a9238def923cbe1400e3a32b9
Igor10255 <igor10255@gmail.com>
2016-06-10 03:05:09 +0200

    Całe priorytety są gotowe, dodane role

--- app/src/main/java/com/example/bidzis/rsystem/Role/RoleSave.java
+++ app/src/main/java/com/example/bidzis/rsystem/Role/RoleSave.java
@@ -1,12 +1,42 @@
 package com.example.bidzis.rsystem.Role;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.bidzis.rsystem.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class RoleSave extends AppCompatActivity {
 
@@ -15,45 +45,38 @@ public class RoleSave extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role_save);
 
-        final EditText  etRoleName = (EditText) findViewById(R.id.etRoleName);
-        final EditText etPermissionName = (EditText) findViewById(R.id.etRolePermissionName);
+       // final EditText  etRoleName = (EditText) findViewById(R.id.etRoleName);
+       // final EditText etPermissionName = (EditText) findViewById(R.id.etRolePermissionName);
 
-        final Button btSave = (Button) findViewById(R.id.btRoleSave);
-/*
+       // final Button btSave = (Button) findViewById(R.id.btRoleSave);
+
         final JSONArray[] jsonArray = {null};
         final Map<String, String> map = new HashMap<String, String>();
-        final EditText etDescription = (EditText) findViewById(R.id.etDescriptionHistorySaveWithAttachment);
-        final Button btSave = (Button) findViewById(R.id.btSaveHistorySaveWithAttachment);
+        final EditText  etRoleName = (EditText) findViewById(R.id.etRoleName);
+        final Button btSave = (Button) findViewById(R.id.btRoleSave);
+        final ListView lvListView8  = (ListView) findViewById(R.id.listView8);
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
-        final String user = "GandalfTheGray";
+       // final String user = "GandalfTheGray";
         final String[] idAttachment = {""};
         final boolean[] flaga = {false};
 
-        final Spinner spinner = (Spinner) findViewById(R.id.spinnerTypeHistorySaveWithAttachment);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.type_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+
         String jsonString = "{\n" +
-                "  \"id\": \"0\", +
-                "  \"name\": \"string\"," +"
-                "  \"permissions\": [\n" +
+                "  \"id\": \"0\"," +
+                "  \"name\": \"string\"," +
+                "  \"permissions\": [\"" +
                 "    {\n" +
-                "      \"id\": 0\n" +
-                "    }\n" +
+                "      \"id\": \"51\"\n" +
+                "    }\n"  +
                 "  ],\n" +
-                "  \"type\": \"WEWNETRZNY\",\n" +
-                "  \"user\": \"string\"\n" +
                 "}";
-        {
-                "name": "string",
-                "permissions": [
-            {
-                "id": 51
-            }
-            ]
-        }
+
         JSONObject json = null;
         try {
             json = new JSONObject(jsonString);
@@ -73,9 +96,9 @@ public class RoleSave extends AppCompatActivity {
                     }
                     try {
                         assert finalJson != null;
-                        assert etDescription != null;
-                        finalJson.put("description", etDescription.getText().toString());
-                        finalJson.put("user", user);
+                        assert etRoleName != null;
+                        finalJson.put("name", etRoleName.getText().toString());
+                        //finalJson.put("user", user);
                         JSONArray jsonArray1 = new JSONArray();
                         String ajdik = "{\n" +
                                 "      \"id\": 0\n" +
@@ -88,7 +111,7 @@ public class RoleSave extends AppCompatActivity {
                         e.printStackTrace();
                     }
 
-                    String url = getString(R.string.ip) + "/projektz/histories/saveHistoryWithAttachments";
+                    String url = getString(R.string.ip) + "/projektz/roles/saveRole";
                     JsonObjectRequest request = new JsonObjectRequest
                             (Request.Method.POST, url, finalJson, new Response.Listener<JSONObject>() {
 
@@ -96,7 +119,7 @@ public class RoleSave extends AppCompatActivity {
                                 @Override
                                 public void onResponse(JSONObject response) {
 
-                                    Toast.makeText(getApplicationContext(), "History Added",
+                                    Toast.makeText(getApplicationContext(), "Something Added",
 
                                             Toast.LENGTH_LONG).show();
 
@@ -129,13 +152,13 @@ public class RoleSave extends AppCompatActivity {
                     requestQueue.add(request);
                 }
                 else {
-                    Toast.makeText(getApplicationContext(), "You have to add the attachment",
+                    Toast.makeText(getApplicationContext(), "You have to save the role",
                             Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        String url = getString(R.string.ip) + "/projektz/attachments/getAllAtachments";
+        String url = getString(R.string.ip) + "/projektz/roles/getAll";
         JsonArrayRequest request2 = new JsonArrayRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
 
@@ -155,14 +178,14 @@ public class RoleSave extends AppCompatActivity {
                                 }
                             }
                         }
-                        final ListView listview = (ListView) findViewById(R.id.lvHistorySaveWithAttachment);
+                        final ListView listview = (ListView) findViewById(R.id.listView8);
                         Iterator it = value.iterator();
 
                         final ArrayList<String> list = new ArrayList<String>();
                         while (it.hasNext()) {
                             list.add((String) it.next());
                         }
-                        final StableArrayAdapter adapter = new StableArrayAdapter(HistorySaveWithAttachmentActivity.this,
+                        final StableArrayAdapter adapter = new StableArrayAdapter(RoleSave.this,
                                 android.R.layout.simple_list_item_1, list);
                         assert listview != null;
                         listview.setAdapter(adapter);
@@ -235,21 +258,6 @@ public class RoleSave extends AppCompatActivity {
         public boolean hasStableIds() {
             return true;
         }
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
 
 
 
